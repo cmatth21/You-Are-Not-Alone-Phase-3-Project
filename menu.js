4a259f498022b5a17a015d52bb4806866220f99b
cmatth21 <cmatth21@jhu.edu>
2021-10-14 20:29:16 -0400

    modal added

--- menu.js
+++ menu.js
@@ -7,3 +7,21 @@ function myFunction() {
       x.className = "topnav";
     }
   }
+// Modal
+// Get the modal
+var modal = document.getElementById("myBreathmodal");
+window.onload = function(event){
+    modal.style.display = 'show';
+}
+
+// Close Modal
+var span = document.getElementsByClassName("close")[0];
+span.onclick = function() {
+    modal.style.display = "none";
+}
+// Closes if click outside of modal not working
+window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
